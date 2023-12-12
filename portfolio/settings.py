dfa2de3d0f5631ba4f4dae12a2c128774890a918
houssem9017 <houssem.darragi@esprit.tn>
2023-12-12 03:03:18 +0100

    mysql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT', default=3306, cast=int),
     }
 }
 
