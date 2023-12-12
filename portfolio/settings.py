3eff63cd551d7bae24f2dada94ea10742c695d86
houssem9017 <houssem.darragi@esprit.tn>
2023-12-12 04:37:33 +0100

    staticfiles

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
@@ -118,8 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_URL = 'https://res.cloudinary.com/drjr2cpgn/raw/upload/v1702351857/static/assets/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
