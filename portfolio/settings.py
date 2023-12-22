c89367b7062fa54940b3e17a6dd300baec26dfff
houssem9017 <houssem.darragi@esprit.tn>
2023-12-23 00:46:42 +0100

    build.sh

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -106,14 +106,11 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'https://res.cloudinary.com/drjr2cpgn/raw/upload/v1702351857/static/assets/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# Configures the staticfiles directory to serve
+# static files from /static/ on our deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
